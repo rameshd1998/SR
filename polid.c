e4336212048f464bfd4129c075ad07770724b36d
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-21 10:16:10 +0530

    polid.c

---
+++ polid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int b,s=0,temp;
+    printf("Enter the number:\n");
+    scanf("%d",&b);
+    temp=b;
+    while(temp!=0)
+    {
+        s=s*10;
+        s=s+temp%5;
+        temp=temp/5;
+    }
+    if(b==s)
+    printf("%d is a palidrome",b);
+    else
+    printf("%d is not a palindrome",b);
+    return;
+}
