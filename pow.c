f541c26492468c71ae2ed7498c5b46ea4a78f743
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-21 09:59:42 +0530

    pow.c

---
+++ pow.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+  #include<math.h>
+  void main()
+  {
+  int d,e,result;
+  printf("\n enter the number");
+  scanf("%d",&d);
+  printf("\n enter the power val");
+  scnaf("%d",&e);
+  result=pow(d,e);
+  printf("\n result");
+  }
