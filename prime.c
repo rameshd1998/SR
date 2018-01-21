e00a58a7befc8ef396e633c92dd3aeeb354517c7
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-21 11:28:16 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<5;++)
+{
+   if((n%i)==0)
+     ++count;
+  }
+  if(count==0)
+{
+printf("a prime number");
+}
+else
+{
+printf("a not prime numbeer");
+}
+return 0;
+}
