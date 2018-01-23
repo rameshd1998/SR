ef91b1bccdfb01401236162adaf1dd5db45f5800
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-23 16:34:46 +0530

    two interval.c

---
+++ two interval.c
@@ -0,0 +1,26 @@
+include<stdio.h>
+int main()
+{
+int a,b,i,flag;
+{
+printf("enter the range");
+scanf("%d%d",&a,&b);
+printf("primr no between %d and %d are:",a,b);
+while(a<b)
+{
+flag=0;
+for(1=2;i=a/2;i++)
+{
+if (a%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+printf("%d",a);
+++a;
+}
+getch();
+}
+
