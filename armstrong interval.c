102880bdcfd5f1e0a9faca5111914630292ae80b
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-23 16:47:42 +0530

    armstrong interval.c

--- armstrong interval.c
+++ armstrong interval.c
@@ -6,7 +6,6 @@ printf("Enter the min and max value");
 scanf("%d %d",&min,&max);
 for(i=min;i<max;i++)
 {
-printf("Enter the number");
 scanf("%d",&n);
 while(n!=0)
 {
@@ -15,9 +14,8 @@ cube=rem*rem*rem;
 sum=sum+cube;
 n=n/10;
 }
-}
 if(sum==n)
 printf("It is an amstrong number %d",n);
 else
 printf("It is not an amstrong number %d",n);
-}
+}}
