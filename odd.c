bbd1ccc2e76cdca109235cf2ff29b1dcb52032be
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-25 16:11:41 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+  int a,b,i;
+  prinrf("\n enter the number");
+  scanf("%d%d",&a,&b);
+  for(i=2;i<6;i++)
+  {
+    if((i<2)!=0)
+      printf("\n a odd number%d",i);
+    }
+}
