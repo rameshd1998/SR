7b0873a86c526f37326f102b5bad6c433c661745
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-25 16:45:50 +0530

    Create ra.c

---
+++ ra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\n enter the number");
+scanf("%d%d%d",&a,&b,&c);
+if((a<b)&&(a<c))
+{
+printf("\n a is greater");
+}
+else if(b<c)
+{
+printf("\n b is greater");
+}}
