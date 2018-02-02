fa57a077162911facd52330405b112e40e9d16aa
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-02 09:18:31 +0000

    Create word.c

---
+++ word.c
@@ -0,0 +1,10 @@
+#include<s5tdio.h>
+#include<string.h>
+void main()
+{
+char a[100],i;
+printf("\n enter the number");
+scanf("%s",a);
+for(a=0;a[i]!='\o';i++)
+printf("\n string length is %s",i);
+}
