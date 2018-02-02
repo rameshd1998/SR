d958b1b41f7758a64cff4aea0580ac7deb0beee5
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-02 09:10:22 +0000

    Update str1.c

--- str1.c
+++ str1.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+int main()
 {
-char str[100];
-int l;
+char str[200];
+int m;
 printf("\n enter the string");
 gets(str);
-l=strlen(str);
-printf("\n the total length is %d",l);
+m=strlen(str);
+printf("\n the total length is %d",m);
 }
