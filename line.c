d96e0967d411711877406ab2df53b045671f7293
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-06 09:10:03 +0000

    Create line.c

---
+++ line.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[10],n;
+int i,count;
+printf("\n enter the lines");
+scanf("%s",&a[i]);
+for(i=0;i<2;i++)
+{
+if(a[i]=='.')
+{
+count++;
+}
+printf("%d",count);
+}
+}
