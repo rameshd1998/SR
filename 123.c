2300bf2e4ae65d9bf7e9ab20dff9747eb1fb6780
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-06 09:18:25 +0000

    Create 123.c

---
+++ 123.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[200],n;
+int i,count;
+printf("\n enter the number");
+scanf("%d",&a[i]);
+for(i=0;i<3;i++)
+{
+if(a[i]=='.')
+{
+count++;
+}
+printf("%d",count);
+}
+}
