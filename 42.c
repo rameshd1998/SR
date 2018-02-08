f1c4ce4a111981a0c8c232b68a80d7219239672f
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-08 09:29:58 +0000

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],b[10];
+printf("\nenter the first string");
+scanf("%d",&a);
+printf("\n enter the second string");
+scanf("%d",&b);
+if((a,b)==0)
+{
+printf("\n string are equal);
+else
+{
+printf("\n string is not equal");
+}
+}
+}
