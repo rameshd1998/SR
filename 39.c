0c3c049ec58484ce2f597d7da4fafc0020dbc9f3
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-08 08:48:19 +0000

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,b;
+printf("\n enter ten  number");
+scanf("%d",&a[i]);
+for(i=0;i<10;i++)
+{
+b=a[i];
+for(i=0;i<10;i++)
+{
+if(a[i]>b)
+{
+b=a[i];
+printf("greatest ten number",b);
+return 0;
+}
+}
+}
+}
