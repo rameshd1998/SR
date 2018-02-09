2ac52df3bff86ba6e19073303804017477dd7993
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-09 08:47:31 +0000

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str,a,b;
+printf("\n enter the string1");
+gets(a);
+printf("\n string2");
+gets(b);
+if((a,b)==0)
+{
+printf("\n %s %s",a,b);
+}
+}
