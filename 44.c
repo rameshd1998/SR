cf537394437ddea8f573b4b86d5106f9cb76abfb
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-09 08:57:25 +0000

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+if(i<n)
+{
+    printf("\n yes");
+    }else
+    {
+        printf("\n no");
+    }
+}
+}
