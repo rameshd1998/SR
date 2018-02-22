eff25d293334270d1da9aadd216090905d825b07
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-22 16:42:09 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+int num,num1=0;
+printf("\ enter the number");
+scanf("%d",&num);
+printf("%d",num");
+if(num/2==0)
+{
+num1=num-1;
+printf("\n enter the less number",num1);
+}
+else
+{
+num1=num-2;
+printf("\n enter the less number",num1);
+}
+return 0;
+}
