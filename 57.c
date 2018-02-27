b7cfee03fc5c4fe83ac0938dade541ad3656b53d
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-02-27 16:26:21 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+printf("\n enter the number1");
+scanf("%d",&num1);
+printf("\n enter the number2");
+scanf("%d",&num2);
+if(num1<num2)
+{
+printf("before swapping %d%d",num1,num2);
+}
+else
+{
+    printf("\n after swapping%d%d",num1,num2);
+}
+}
+
