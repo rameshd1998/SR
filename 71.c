72448af78614b843a4d558d3913a5fb4a9a83083
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-03-08 15:58:07 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the string")
+gets(a);
+if(a==strrev(a))
+{
+printf("\n is palindrome");
+}
+else
+{
+printf("\n is not a palindrome");
+}
+}
