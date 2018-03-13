3e88f5e0daca13117c17182116e17cede5cc129a
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-03-13 16:29:21 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f;
+printf("\n enter the number");
+scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+a=b-c;
+b=c-d;
+d=e-f;
+printf("%d %d %d",a,b,d);
+}
